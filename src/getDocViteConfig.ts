619062951a08e54c52edfffa015c3041fb26c212
Guo Yunhe <i@guoyunhe.me>
2023-02-18 19:49:10 +0800

    package alias

--- src/getDocViteConfig.ts
+++ src/getDocViteConfig.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
+import glob from 'fast-glob';
 import { UserConfig } from 'vite';
 import { getPackageJson } from './getPackageJson';
 
 export async function getDocViteConfig(): Promise<UserConfig> {
+  const entries = await glob('src/index.{js,jsx,ts,tsx}');
   const packageJson = await getPackageJson();
   return {
     build: {
@@ -13,5 +15,10 @@ export async function getDocViteConfig(): Promise<UserConfig> {
       PACKAGE_VERSION: `"${packageJson.version}"`,
     },
     plugins: [react({ tsDecorators: true })],
+    resolve: {
+      alias: {
+        [packageJson.name]: entries[0],
+      },
+    },
   };
 }
