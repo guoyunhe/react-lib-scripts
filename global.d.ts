43cf737ea82f374685227ddc44deacdeeba8e9ec
Guo Yunhe <i@guoyunhe.me>
2023-02-20 20:10:29 +0800

    loader type

--- global.d.ts
+++ global.d.ts
@@ -3,4 +3,14 @@ declare global {
   const PACKAGE_VERSION: string;
 }
 
+declare module '*?raw' {
+  const content: string;
+  export default content;
+}
+
+declare module '*?url' {
+  const content: string;
+  export default content;
+}
+
 export {};
