f282429299012dbf67174ad0634d73286b9d5f6d
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2023-02-22 10:49:26 +0800

    assets types

---
+++ assets.d.ts
@@ -0,0 +1,59 @@
+declare module '*.module.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.module.less' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.module.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.txt' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.ejs' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: string;
+  export default content;
+}
+
+declare module '*?raw' {
+  const content: string;
+  export default content;
+}
+
+declare module '*?url' {
+  const content: string;
+  export default content;
+}
