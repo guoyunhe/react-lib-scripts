0606e376ec807ca57ee80441c6a1d5db2ec8f07c
Guo Yunhe <i@guoyunhe.me>
2024-01-07 14:41:39 +0800

    mdx

--- src/getDocViteConfig.ts
+++ src/getDocViteConfig.ts
@@ -1,19 +1,12 @@
-import react from '@vitejs/plugin-react-swc';
+import doc from '@rive/doc-vite-plugin';
 import { UserConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { getPackageJson } from './getPackageJson';
 
 export async function getDocViteConfig(): Promise<UserConfig> {
-  const packageJson = await getPackageJson();
   return {
     base: './', // for GitHub pages
     build: {
       outDir: 'build',
     },
-    define: {
-      PACKAGE_NAME: `"${packageJson.name}"`,
-      PACKAGE_VERSION: `"${packageJson.version}"`,
-    },
-    plugins: [react({ tsDecorators: true }), tsconfigPaths()],
+    plugins: [doc()],
   };
 }
